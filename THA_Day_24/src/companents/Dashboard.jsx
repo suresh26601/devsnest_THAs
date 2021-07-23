461e62135effbc2b6d7b132e966089b0fbb0439e
Suresh <sureshkumawat26601@gmail.com>
2021-07-23 17:32:57 +0530

    completed also using creatContext

--- THA_Day_24/src/companents/Dashboard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Dashboard = () => {
-    return (
-        <div>
-           <h3>Dashboard: Private Route</h3>
-        </div>
-    )
-}
--- THA_Day_24/src/companents/Profile.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Profile = () => {
-    return (
-        <div>
-            <h3>Profile</h3>
-        </div>
-    )
-}
